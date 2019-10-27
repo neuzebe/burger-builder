61135c0cec440c19c2a09dbff46e6c3ebd7a5fe0
Noel Euzebe <noel.euzebe@gmail.com>
2019-10-26 22:47:59 -0400

    Initial version of Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
         <Aux>
           <div>
-            Toolbar, SideDrawer, Backdrop
+            <Toolbar />
+            SideDrawer
           </div>
           <main className={classes.Content}>
             {props.children}
