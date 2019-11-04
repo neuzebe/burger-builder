0ceea74de154e3247d2d21525f688b57e7e332a1
Noel Euzebe <noel.euzebe@gmail.com>
2019-11-04 00:04:56 -0500

    Add logic to sidebar to show/hide

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
+class Layout extends Component{
+  state = {
+    showSidebar: true
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSidebar: false})
+  };
+
+  render(){
+    return (
         <Aux>
           <div>
             <Toolbar />
-            <SideDrawer/>
+            <SideDrawer open={this.state.showSidebar} closed={this.sideDrawerClosedHandler} />
           </div>
           <main className={classes.Content}>
-            {props.children}
+            {this.props.children}
           </main>
         </Aux>
-);
+    )
+  }
+}
 
-export default layout;
+export default Layout;
