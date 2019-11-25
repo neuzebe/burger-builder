0063a66a34402758be8653ee659d912b2c3f1535
Noel Euzebe <noel.euzebe@gmail.com>
2019-11-24 23:28:06 -0500

    move Ordering behaviour to Redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,8 @@ import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import { connect } from 'react-redux';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
   state = {
@@ -99,23 +101,13 @@ class ContactData extends Component {
       formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
     }
 
-    this.setState({loading: true});
     const order = {
       ingredients: this.props.ings,
       price:       this.props.price,
       orderData:   formData,
     };
 
-    axios.post('/orders.json', order)
-          .then(response => {
-            console.log(response);
-            this.setState({loading: false});
-            this.props.history.push('/');
-          })
-          .catch(error => {
-            console.log(error);
-            this.setState({loading: false})
-          })
+    this.props.onOrderBurger(order);
   };
 
   inputChangedHandler = (event, inputIdentifier) => {
@@ -212,4 +204,8 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData)),
+}
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
