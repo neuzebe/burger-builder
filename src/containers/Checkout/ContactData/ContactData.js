41fb297a9bc00a0a9949f4ba64c2e0bd5c266425
Noel Euzebe <noel.euzebe@gmail.com>
2019-11-24 23:51:42 -0500

    CombineReducers
    Minor fixes: typo fetching name from response
    not sending delivery method in post request

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,7 +85,8 @@ class ContactData extends Component {
           options: [{value: 'fastest', displayValue: 'Fastest'}, {value: 'cheapest', displayValue: 'Cheapest'}]
         },
         valid: true,
-        value: ''
+        validation: {},
+        value: 'fastest',
       },
 
     },
@@ -198,9 +199,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    loading: state.loading,
+    ings:    state.burgerBuilder.ingredients,
+    price:   state.burgerBuilder.totalPrice,
+    loading: state.order.loading,
   }
 }
 
