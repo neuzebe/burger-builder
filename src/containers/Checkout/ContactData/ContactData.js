de83376fbe1278f0458073d7aa150653f72cabc4
Noel Euzebe <noel.euzebe@gmail.com>
2019-11-24 23:43:45 -0500

    Update loading logic on ContactData
    Also, add redirect if ContactData loaded with no ingredients.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -89,7 +89,6 @@ class ContactData extends Component {
       },
 
     },
-    loading: false,
   };
 
   orderHandler = (event) => {
@@ -184,7 +183,7 @@ class ContactData extends Component {
       </form>
     );
 
-    if(this.state.loading){
+    if(this.props.loading){
       form = <Spinner/>
     }
 
@@ -201,11 +200,14 @@ const mapStateToProps = state => {
   return {
     ings: state.ingredients,
     price: state.totalPrice,
+    loading: state.loading,
   }
 }
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData)),
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData)),
+  }
 }
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
