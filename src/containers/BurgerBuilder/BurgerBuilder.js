87e1f3e34e6aa3098dad95b0805a5ff9e657a430
Noel Euzebe <noel.euzebe@gmail.com>
2019-11-24 22:54:09 -0500

    Shifting towards actionCreators approach
    add actionCreators for the synchronous actions add and remove ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions';
+import * as burgerBuilderActions from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
   state = {
@@ -109,8 +109,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIngredientAdded: (name) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: name}),
-    onIngredientRemoved: (name) =>  dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: name}),
+    onIngredientAdded: (name)   => dispatch(burgerBuilderActions.addIngredient(name)),
+    onIngredientRemoved: (name) => dispatch(burgerBuilderActions.removeIngredient(name)),
   }
 };
 
