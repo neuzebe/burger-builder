cea80e4f29dbc8f9f0cf21a9d054ca9d1f85e33f
Noel Euzebe <noel.euzebe@gmail.com>
2019-12-30 13:23:01 -0500

    Guard routes
    stop some routes from being available unless logged in

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -15,17 +15,38 @@ class App extends Component{
   }
 
   render(){
+    let routes = (
+      <Switch>
+        <Route path="/auth" exact component={Auth}/>
+        <Route path="/" exact component={BurgerBuilder}/>
+        <Redirect to='/'/>
+      </Switch>
+    )
+    
+    if(this.props.isAuthenticated){
+      routes = (
+        <Switch>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Route path="/orders" exact component={Orders}/>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/logout" component={Logout}/>
+          <Redirect to='/'/>
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Route path="/" exact component={BurgerBuilder}/>
-          <Route path="/orders" exact component={Orders}/>
-          <Route path="/auth" exact component={Auth}/>
-          <Route path="/checkout" component={Checkout}/>
-          <Route path="/logout" component={Logout}/>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null,
   }
 }
 
@@ -35,4 +56,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
