d104c8a5ce6cdea0088d87f8ae562a5896ee185a
Noel Euzebe <noel.euzebe@gmail.com>
2019-12-30 13:14:09 -0500

    Use localStorage to keep user logged in
    even if page reloads

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React , { Component } from 'react';
+import { connect } from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Route path="/" exact component={BurgerBuilder}/>
-        <Route path="/orders" exact component={Orders}/>
-        <Route path="/auth" exact component={Auth}/>
-        <Route path="/checkout" component={Checkout}/>
-        <Route path="/logout" component={Logout}/>
-      </Layout>
-    </div>
-  );
+class App extends Component{
+  componentDidMount(){
+    this.props.onTryAutoAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Layout>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Route path="/orders" exact component={Orders}/>
+          <Route path="/auth" exact component={Auth}/>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/logout" component={Logout}/>
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoAuth: () => dispatch(actions.authCheckState()),
+  }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
