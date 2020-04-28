39a70c9213c5f6a5cdf9182e1887b10d0caa1ce3
Noel Euzebe <noel.euzebe@gmail.com>
2020-04-28 02:53:44 -0400

    Saga - move check auth timeout to saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,16 +24,21 @@ export const authFail = (error) => {
 };
 
 export const checkAuthTimeout = (expirationTime) => {
-  return dispatch => {
-    setTimeout( () => {
-      dispatch(logout());
-    }, expirationTime * 1000);
+  return {
+    type: actionTypes.AUTH_CHECK_TIMEOUT,
+    expirationTime: expirationTime
   }
 };
 
 export const logout = () => {
   return {
     type: actionTypes.AUTH_INITIATE_LOGOUT
+  }
+}
+
+export const logoutSucceed = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
   }
 }
 
