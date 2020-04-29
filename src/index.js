61126004169ab56b62a5a4533718b6db56c5c7ac
Noel Euzebe <noel.euzebe@gmail.com>
2020-04-29 02:44:58 -0400

    Convert order actions into sagas

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import createSagaMiddleware from 'redux-saga';
 import { 
   watchAuth,
   watchBurgerBuilder,
+  watchOrder,
  } from './store/sagas'
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
@@ -32,6 +33,7 @@ const store = createStore(
 
 sagaMiddleware.run(watchAuth);
 sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
 
 const app = (
   <Provider store={store}>
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -12,6 +12,11 @@ import {
   initIngredientSaga,
 } from './burgerBuilder';
 
+import { 
+  purchaseBurgerSaga,
+  fetchOrdersSaga,
+ } from './order';
+
 export function* watchAuth(){
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
@@ -21,4 +26,9 @@ export function* watchAuth(){
 
 export function* watchBurgerBuilder(){
   yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientSaga);
+}
+
+export function* watchOrder(){
+  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
