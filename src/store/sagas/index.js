150ac61b529e4350cee84776d9f88f5b67bb36a8
Noel Euzebe <noel.euzebe@gmail.com>
2020-05-08 01:27:35 -0400

    takeLatest example

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery, all } from 'redux-saga/effects';
+import { takeEvery, all, takeLatest } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
 import { 
@@ -33,6 +33,7 @@ export function* watchBurgerBuilder(){
 }
 
 export function* watchOrder(){
-  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  // this will auto cancel any other on-going executions and run the newest one
+  yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
   yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
