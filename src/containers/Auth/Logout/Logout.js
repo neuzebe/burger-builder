d9d6b44cf62956c90946bbc341c0455ec1fdc406
Noel Euzebe <noel.euzebe@gmail.com>
2020-05-08 02:18:00 -0400

    convert Logout to functional

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import * as actions from '../../../store/actions/index';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
-class Logout extends Component {
-  componentDidMount() {
-    this.props.onLogout();
-  }
+const Logout = props => {
+  useEffect( () => {
+    props.onLogout();
+  }, []);
 
-  render(){
-    return <Redirect to="/" />;
-  }
+  return <Redirect to="/" />;
 }
 
 const mapDispatchToProps = dispatch => {
